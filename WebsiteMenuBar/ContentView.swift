14b7d2531d0d02e8ff3439350242174addc5cd8c
junha <goodjunha@gmail.com>
2023-10-31 18:49:42 +0900

    ⚙️:: 프로젝트 세팅

---
+++ WebsiteMenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebsiteMenuBar
+//
+//  Created by 박준하 on 10/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebsiteMenuBar/WebsiteMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsiteMenuBarApp.swift
+//  WebsiteMenuBar
+//
+//  Created by 박준하 on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WebsiteMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
